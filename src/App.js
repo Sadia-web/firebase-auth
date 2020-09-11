6c1c32dc79d12210724b1764dad31db8d76ff3ee
Sadia-web <sadiamim354@gmail.com>
2020-09-11 20:02:44 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,162 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import * as firebase from "firebase/app";
 import './App.css';
+import "firebase/auth";
+import firebaseConfig from './firebaseConfig';
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [newUser, setNewUser] = useState(false);
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name:'',
+    email:'',
+    password:''
+  })
+
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+  const fbProvider = new firebase.auth.FacebookAuthProvider();
+  const handleSignIn = () =>{
+    firebase.auth().signInWithPopup(googleProvider)
+    .then(res => {
+      const {displayName, email} = res.user;
+      const signedInUser = {
+        isSignedIn: true,
+        name: displayName,
+        email: email
+      };
+      setUser(signedInUser);
+    })
+    .catch(err => {
+      console.log(err);
+      console.log(err.message);
+    })
+  }
+  const handleFBLogin = () => {
+    firebase.auth().signInWithPopup(fbProvider).then(function(result) {
+      var token = result.credential.accessToken;
+      var user = result.user;
+      console.log('fb user after sign in', user)
+    }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode, errorMessage)
+    });
+  }
+  const handleSignOut = () => {
+    firebase.auth().signOut()
+    .then(res => {
+      const signedOutUser = {
+        isSignedIn: false,
+        name: '',
+        email:'',
+        error:'',
+        success: false
+      }
+      setUser(signedOutUser);
+    })
+    .catch(err => {
+
+    });
+  }
+  
+  const handleBlur = (e) => { //e means event
+    let isFieldValid = true;
+    if(e.target.name === 'email'){
+      isFieldValid = /\S+@\S+\.\S+/.test(e.target.value);
+    }
+    if(e.target.name === 'password'){
+      const isPasswordValid = e.target.value.length > 6;
+      const passwordHasNumber = /\d{1}/.test(e.target.value);
+      isFieldValid = isPasswordValid && passwordHasNumber;
+    }
+    if(isFieldValid){
+      const newUserInfo = {...user};
+      newUserInfo[e.target.name] = e.target.value;
+      setUser(newUserInfo);
+    }
+  }
+  const handleSubmit = (e) => {
+    //console.log(user.email, user.password);
+    if(newUser && user.email && user.password){
+      firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+      .then( res => {
+        const newUserInfo = {...user};
+        newUserInfo.error = '';
+        newUserInfo.success = true;
+        setUser(newUserInfo);
+        updateUserName(user.name);
+      })
+      .catch(error => {
+        const newUserInfo = {...user};
+        newUserInfo.error = error.message;
+        newUserInfo.success =  false;
+        setUser(newUserInfo);
+      });
+    }
+
+    if(!newUser && user.email && user.password){
+      firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+      .then(res => {
+        const newUserInfo = {...user};
+        newUserInfo.error = '';
+        newUserInfo.success =  true;
+        setUser(newUserInfo);
+        console.log('sign in user info', res.user);
+      })
+      .catch(function(error) {
+        const newUserInfo = {...user};
+        newUserInfo.error = error.message;
+        newUserInfo.success =  false;
+        setUser(newUserInfo);
+      });
+    }
+    e.preventDefault();
+  }
+
+  const updateUserName = name => {
+    const user = firebase.auth().currentUser;
+    user.updateProfile({
+    displayName: name      
+    }).then(function() {
+      console.log('User name updated successfully')  
+    }).catch(function(error) {
+      console.log(error)
+  });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {
+        user.isSignedIn ? <button onClick={handleSignOut} >Sign out</button>:
+        <button onClick={handleSignIn} >Sign in</button>
+      }
+      <br/>
+      {
+        <button onClick={handleFBLogin}>Sign in using Facebook</button>
+      }
+      {
+        user.isSignedIn && <div>
+          <p> Welcome ,{user.name}!</p>
+          <p>Your email: {user.email}</p>
+          </div>
+      }
+      <h1>Our Own Authentication</h1>
+      <input type="checkbox" onChange={() => setNewUser(!newUser)} name="newUser" id=""/>
+      <label htmlFor="newUser">New User Sign up</label>
+      <form onSubmit={handleSubmit}>
+      {newUser && <input type="text" name="name" onBlur={handleBlur} placeholder="Your Name"/>}
+      <br/>
+      <input type="text" name="email" onBlur={handleBlur} placeholder="Enter Email Address" required/>
+      <br/>
+      <input type="password" name="password" onBlur={handleBlur} placeholder="Enter Password" required/>
+      <br/>
+      <input type="submit" value={newUser ? 'Sign up' : 'Sign in'}/>
+      </form>
+      <p style={{color: 'red'}}>{user.error}</p>
+      {user.success && <p style={{color: 'green'}}>User {newUser ? 'created' : 'Logged in'} Successfully!</p>}
+    </div>    
   );
 }
 
---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA4-QFMXlPRvjyzmDJgXaEYLNwJFdCtgBI",
+    authDomain: "ema-john-simple-30309.firebaseapp.com",
+    databaseURL: "https://ema-john-simple-30309.firebaseio.com",
+    projectId: "ema-john-simple-30309",
+    storageBucket: "ema-john-simple-30309.appspot.com",
+    messagingSenderId: "427466092865",
+    appId: "1:427466092865:web:26c7f64b749fd673ec31c0"
+  };
+export default firebaseConfig;
